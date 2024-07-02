a40d2ae7faaf522b954c040b373e065227d7615a
seongeun223 <jklun080921@gmail.com>
2024-07-02 17:35:14 +0900

    chap04

--- chap04-request-lecture-source/src/main/java/com/ohgiraffers/section01/headers/RequestHeaderPrintServlet.java
+++ chap04-request-lecture-source/src/main/java/com/ohgiraffers/section01/headers/RequestHeaderPrintServlet.java
@@ -18,7 +18,7 @@ public class RequestHeaderPrintServlet extends HttpServlet {
     * 2. Request header
     *       Fetch될 리소스나 클라이언트 자체에 대한 상세 정보를 포함하는 헤더
     * 3. Response header
-    *       위치나 서버 자체와 같은 응답헤 대한 부가적인 정보를 갖는 헤더
+    *       위치나 서버 자체와 같은 응답에 대한 부가적인 정보를 갖는 헤더
     * 4. Entity header
     *       컨텐츠의 길이나 MIME 타입과 같은 엔티티 body에 대한 상세 정보를 포함하는 헤더
     *       (요청 응답 모두 사용되며, 메시지 바디의 컨텐츠를 나타내기에 GET요청은 해당되지 않는다.)
