a1d1e5db606599af6ddfb27dc76d0a42a64553ef
seongeun223 <jklun080921@gmail.com>
2024-07-03 17:56:16 +0900

    chap11

--- chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RequestWrapper.java
+++ chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RequestWrapper.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletRequestWrapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Scanner;
+
 public class RequestWrapper extends HttpServletRequestWrapper {
 
     // HttpServletRequestWrapper는 기본생성자가 없기 때문에, request를 전달해주는 생성자가 필요하다.
@@ -22,10 +24,16 @@ public class RequestWrapper extends HttpServletRequestWrapper {
             value = passwordEncoder.encode(super.getParameter(key));
 
             System.out.println("value ================>" + value);
-        } else {
-            value = super.getParameter(key);
-        } if("name".equals(key)){
+        } else if("name".equals(key)){
+//            Scanner sc = new Scanner(System.in);
+//            value = sc.nextLine();
+//            System.out.println("value = " + value);
 
+            System.out.println("이름 변경 필터 적용");
+            value = "홍길동";
+            System.out.println("value : " + value);
+        } else{
+            value = super.getParameter(key);
         }
         return value;
     }
